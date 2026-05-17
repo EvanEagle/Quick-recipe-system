e06542d62bdc3132c4e0db4179fda3f719ac0c21
Evan <evan@example.com>
2026-05-17 10:04:39 +0800

    進階專案-快速料理小幫手初始檔-已設定 pom.xml 及 yml檔

---
+++ src/main/java/com/example/quick_recipe_system/QuickRecipeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.quick_recipe_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickRecipeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickRecipeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quick_recipe_system/QuickRecipeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick_recipe_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickRecipeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
